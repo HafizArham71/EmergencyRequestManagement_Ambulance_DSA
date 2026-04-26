3541d26b3b2c7a014295a038d8cec74f78048398
HafizArham71 <hafizarhammujahid71@gmail.com>
2026-04-26 18:38:45 +0500

    initial

--- src/EmergencyStatus.java
+++
@@ -1,5 +0,0 @@
-public enum EmergencyStatus {
-
-    // Attributes
-    HEART_ATTACK, ACCIDENT, INJURY, FEVER;
-}
--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,7 @@ public class Main {
 
     static void main(String[] args) {
 
-        Request r1 = new Request("Rahul1", new Location("College Stop", "Shalimar Garden", "Lahore", "Pakistan"), EmergencyStatus.FEVER, 1);
-
-        Request r2 = new Request("Rahul2", new Location("College Stop", "Shalimar Garden", "Lahore", "Pakistan"), EmergencyStatus.FEVER, 1);
-
-        Request r3 = new Request("Rahul3", new Location("College Stop", "Shalimar Garden", "Lahore", "Pakistan"), EmergencyStatus.FEVER, 1);
-
-        Request r4 = new Request("Rahul4", new Location("College Stop", "Shalimar Garden", "Lahore", "Pakistan"), EmergencyStatus.FEVER, 1);
-
-        Request r5 = new Request("Rahul5", new Location("College Stop", "Shalimar Garden", "Lahore", "Pakistan"), EmergencyStatus.FEVER, 1);
-
-        System.out.println(r1);
-        System.out.println(r2);
-        System.out.println(r3);
-        System.out.println(r4);
-        System.out.println(r5);
-    }
-
-    static int generatePriority(EmergencyStatus status) {
-        if(status == EmergencyStatus.HEART_ATTACK) {
-            if(heartAttack == 0 || )
-                heartAttack++;
-            else if (heartAttack)
-        }
+        System.out.println("Hello!");
 
     }
 }
